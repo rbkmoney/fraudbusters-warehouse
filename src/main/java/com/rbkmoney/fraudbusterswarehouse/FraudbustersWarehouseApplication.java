24c9167e8cfa20773857d381b4cef6a648812296
ggmaleva <griwania@gmail.com>
2021-08-11 13:42:27 +0300

    add clickhouse

--- src/main/java/com/rbkmoney/fraudbusterswarehouse/FraudbustersWarehouseApplication.java
+++ src/main/java/com/rbkmoney/fraudbusterswarehouse/FraudbustersWarehouseApplication.java
@@ -2,10 +2,12 @@ package com.rbkmoney.fraudbusterswarehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @ServletComponentScan
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class FraudbustersWarehouseApplication extends SpringApplication {
 
     public static void main(String[] args) {
